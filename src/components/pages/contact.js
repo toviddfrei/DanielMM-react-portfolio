ade0bcd61e1d764052677fe708316ca365a601b7
toviddfrei <149096220+toviddfrei@users.noreply.github.com>
2024-10-12 18:12:01 +0200

    modify page contact and style portfolio

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,11 +1,45 @@
 import React from "react";
-import moment from "moment";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import contactPagePicture from "../../../static/assets/images/auth/login.jpg";
 
 export default function() {
-  return <div>
-            <h1>TOVID DFREI- Portfolio</h1>
-            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
-            Contact
-          </div>;
-  
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + contactPagePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }}
+      />
+      <div className="right-column">
+        <div className="contact-bullet-points">
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="phone" />
+            </div>
+
+            <div className="text">555-555-5555</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="envelope" />
+            </div>
+
+            <div className="text">toviddfrei@example.com</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="map-marked-alt" />
+            </div>
+
+            <div className="text">Bilbao, Spain</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
